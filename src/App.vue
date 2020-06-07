fb4425dfba1a075d64eb0722bb037087d4202143
jone <876573176@qq.com>
2020-06-07 21:35:14 +0800

    jonetest

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,32 @@
 <template>
   <div id="app">
     <div id="nav">
-      {{$store.state.age}}<br>
-      我的年龄{{$store.getters.getAge}}
+      {{ $store.state.age }}<br />
+      我的年龄嗯{{ $store.getters.getAge }}
       <button @click="add">add</button>
       <button @click="min">min</button>
-      {{$store.state.a.x}}<br>
-      {{$store.state.b.y}}
+      {{ $store.state.a.x }}<br />
+      {{ $store.state.b.y }}
     </div>
-
   </div>
 </template>
 <script>
 export default {
-  name:'app',
-  methods:{
-    add(){
-      this.$store.commit('syncAdd',90)
+  name: "app",
+  methods: {
+    add() {
+      this.$store.commit("syncAdd", 90);
     },
-    min(){
-      this.$store.dispatch('asyncMin',50)
-    }
-  }
-}
+    min() {
+      this.$store.dispatch("asyncMin", 50);
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
